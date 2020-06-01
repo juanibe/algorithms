fd28bbe6c2db18b26d8d98570f7e114288fc3a52
Juan Ignacio Benito <benitojuanignacio@gmail.com>
2020-06-01 02:14:42 +0100

    [Add] Example

--- multiple_pointers.js
+++ multiple_pointers.js
@@ -214,3 +214,27 @@ function averagePair(arr, avNum) {
     }
     return false;
 }
+
+/*
+    Write a function called isSubsequence which takes in two strings and checks 
+    whether the characters in the first string form a subsequence of the characters
+    in the second string. In other words, the function should check whether the characters
+    in the first string appear somewhere in the second string withour their order changing.
+*/
+function isSubsequence(str1, str2) {
+    let i = 0;
+    let j = 0;
+
+    while (j < str2.length) {
+        if (str1[i] === str2[j]) {
+            i++;
+        }
+
+        if (i === str1.length) {
+            return true;
+        }
+
+        j++;
+    }
+    return false;
+}

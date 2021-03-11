366c09dd59d46131e115b244af0915233905d3f6
Juan Ignacio Benito <juanibe@Juans-Air-2.lan>
2021-03-11 11:41:57 +0000

    [Add] Example

--- sliding_window.js
+++ sliding_window.js
@@ -92,3 +92,50 @@ function maxSubArraySum(arr, sum) {
     return maxSum;
 
 }
+
+/**
+ * Write a function called minSubArrayLen
+ * which accepts two parameters - an array of positive integers and a positive integer. 
+ * This function should return the minimal length of a contiguous
+ * subarray of which the sum is greater than or equal to the integer passed to the function. 
+ * If there isn't one, return 0 instead.
+ * 
+ * Time Complexity - O(n)
+ * Space Complexity - O(1)
+ * 
+ * Example:
+ * minSubArrayLen([2,3,1,2,1,4,3, 7,8,45,11], 7) -> 2, because [4,3] is the smallest sub array.
+ */
+function minSubArrayLen(nums, sum) {
+
+    let total = 0;
+    let start = 0;
+    let end = 0;
+    let minLen = Infinity;
+
+    /**
+     * Iterate over the array for first time.
+     * The start variable will be 0 at first.
+     * During that iteration we will sum every number
+     * and when total reach the value of sum, we stop. If does not reach
+     * that number the window move to the right. 
+     */
+    while (start < nums.length) {
+
+        if (total < sum && end < nums.length) {
+            total += nums[end];
+            end++;
+        }
+
+        else if (total >= sum) {
+            minLen = Math.min(minLen, end - start);
+            total -= nums[start];
+            start++;
+        }
+
+        else {
+            break;
+        }
+    }
+    return minLen === Infinity ? 0 : minLen;
+};
